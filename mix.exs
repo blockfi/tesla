082ad112635b51cabc5f5ca95c75c5a630bafe93
Luke Bakken <luke.bakken@blockfi.com>
2021-04-21 07:17:04 -0700

    Use blockfi fork of finch to be able to use "master" branch of mint.
    Use ssh: and update all deps

--- mix.exs
+++ mix.exs
@@ -57,9 +57,9 @@ defmodule Tesla.Mixfile do
       {:ibrowse, "~> 4.4.0", optional: true},
       {:hackney, "~> 1.6", optional: true},
       {:gun, "~> 1.3", optional: true},
-      {:finch, "~> 0.3", optional: true},
+      {:finch, git: "git@github.com:blockfi/finch.git", branch: "lukebakken/sslkeylogfile-with-mint-master", optional: true},
       {:castore, "~> 0.1", optional: true},
-      {:mint, "~> 1.0", optional: true},
+      {:mint, git: "https://github.com/elixir-mint/mint.git", branch: "main", optional: true},
 
       # json parsers
       {:jason, ">= 1.0.0", optional: true},
